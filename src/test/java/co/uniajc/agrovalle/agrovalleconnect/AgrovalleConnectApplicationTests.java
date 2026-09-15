f9f68a94324ce9f2a76009cc446383b8f57218b0
JD8329 <juan2444zap@gmail.com>
2026-09-14 23:25:17 -0500

    chore: inicializar proyecto Spring Boot con Maven

---
+++ src/test/java/co/uniajc/agrovalle/agrovalleconnect/AgrovalleConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uniajc.agrovalle.agrovalleconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrovalleConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
