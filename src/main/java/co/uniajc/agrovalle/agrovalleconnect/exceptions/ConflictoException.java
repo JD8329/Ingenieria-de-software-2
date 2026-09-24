63d3be390f9c3de24ade4f90f35f250dae514d7d
Nestor Javier Hernandez <hernandezvallecillan@gmail.com>
2026-09-23 21:52:53 -0500

    feat(exceptions): agregar manejo global de errores

---
+++ src/main/java/co/uniajc/agrovalle/agrovalleconnect/exceptions/ConflictoException.java
@@ -0,0 +1,8 @@
+package co.uniajc.agrovalle.agrovalleconnect.exceptions;
+
+public class ConflictoException extends RuntimeException {
+
+    public ConflictoException(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ src/main/java/co/uniajc/agrovalle/agrovalleconnect/exceptions/RecursoNoEncontradoException.java
@@ -0,0 +1,8 @@
+package co.uniajc.agrovalle.agrovalleconnect.exceptions;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+
+    public RecursoNoEncontradoException(String mensaje) {
+        super(mensaje);
+    }
+}
